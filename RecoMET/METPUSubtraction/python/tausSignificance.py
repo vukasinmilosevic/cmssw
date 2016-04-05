1ea2521d6b22424517b22b458e92d896c6b76087
Raphael Friese <raphael.friese@cern.ch>
2016-04-05 13:31:42 +0200

    move training code to MET repo

--- RecoMET/METPUSubtraction/python/tausSignificance.py
+++ RecoMET/METPUSubtraction/python/tausSignificance.py
@@ -2,11 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoMET.METProducers.PFMET_cfi import pfMet
 tauPFMET = pfMet.clone()
-tauPFMET.src = cms.InputTag('tauDecayProducts')
+tauPFMET.src = cms.InputTag('allDecayProducts')
 tauPFMET.alias = cms.string('tauPFMET')
 
 tauDecayProducts = cms.EDProducer("tauDecayProducts",
                          src = cms.InputTag("slimmedTaus") )
+
+allDecayProducts = cms.EDProducer("CandViewMerger", 
+                         src = cms.VInputTag( cms.InputTag("tauDecayProducts"), cms.InputTag("slimmedElectrons"), cms.InputTag("slimmedMuons")))
 
 from PhysicsTools.PatAlgos.producersLayer1.metProducer_cfi import patMETs
 tauMET = patMETs.clone()
